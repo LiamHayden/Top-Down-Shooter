588071f1e31fc2b311903be793b15f6eb5a8e08c
Liam Hayden <65085447+LiamHayden@users.noreply.github.com>
2026-02-05 19:45:39 +0000

    Game Ends When -10 is Reached
    -- Added a check for a score less then or equal to -10.
    -- Added end game if this score is reached.
    -- Refactored GameManager to better manager the game state.
    == Moves methods into GameManager.

--- Assets/Scripts/ProjectileManager.cs
+++ Assets/Scripts/ProjectileManager.cs
@@ -8,7 +8,7 @@ public class ProjectileManager : MonoBehaviour
 
     void Update()
     {
-        if (GameManager.isStarted)
+        if (GameManager.Instance.isStarted)
         {
             FireProjectile();
         }
